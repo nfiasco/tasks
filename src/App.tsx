4357db7374d39a36f7a1cd588075468c4a6f7e0a
Nico Fiasco <nfiasco@udel.edu>
2026-02-03 20:13:49 -0500

    "Add Hello World"

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,8 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                <code>Nico Fiasco</code>
-            </p>
+            <p><code>Nico Fiasco</code></p>
+            <p>Hello World</p>
         </div>
     );
 }
