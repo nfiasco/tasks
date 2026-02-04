83103d96c5213dddc7d8eab19ee399480a877762
Nico Fiasco <nfiasco@udel.edu>
2026-02-04 16:00:44 -0500

    Added: Task 3 - HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,9 +8,62 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+
+            <h1> Task 3: </h1>
+
+            <img
+                src={`${process.env.PUBLIC_URL}/Images/E33.jpg`}
+                alt="Favorite Game"
+                style={{ width: 200 }}
+            />
+
+            <br />
+
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+
+            <ul>
+                <li>I am coding</li>
+                <li>I am doing Task 3</li>
+                <li>I like to eat</li>
+            </ul>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: 100,
+                                height: 100,
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+
+                    <Col>
+                        <div
+                            style={{
+                                width: 100,
+                                height: 100,
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+
+            <br />
+            <p>
+                <strong>Old Tasks:</strong>
+            </p>
             <p>
                 <code>Nico Fiasco</code>
-                </p>
+            </p>
             <p>Hello World</p>
         </div>
     );
